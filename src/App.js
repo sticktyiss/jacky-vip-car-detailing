4b4779c703cc70e72693bf3a8fd89efd1e0015bc
aloratink <sticktyiss@gmail.com>
2023-08-16 19:30:49 -0700

    Setup beginnings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from './components/header';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Footer />
     </div>
   );
 }
