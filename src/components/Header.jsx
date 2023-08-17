0adafab338a808aff2e19e7e8f26642f89a43371
aloratink <sticktyiss@gmail.com>
2023-08-16 20:41:59 -0700

    Working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const Header = () => {
+  // const scrollToSpot = (x, y) => {
+  //   window.scrollTo(x,y);
+  // };
+
   return (
     <header>
-      <h1>Header</h1>
+      <nav>
+        <button onClick={scrollToSpot(0,0)} >Home</button>
+        <button>Reviews</button>
+        <button>Services</button>
+        <button>Location</button>
+      </nav>
+      <h1>
+        VIP Vehicle Detailing <br /> & Power Washing
+      </h1>
     </header>
   );
 };
