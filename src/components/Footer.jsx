c13e7c5e93714357a74c85b466ffbe9ee677eee6
aloratink <sticktyiss@gmail.com>
2023-08-17 21:22:25 -0700

    Added more to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,11 @@ const Footer = () => {
   return (
     <footer>
       <h1>Get in touch</h1>
+      {/* Add icons instead, but this will do for now! */}
+      <p>(505)269-2920</p>
+      <p>VipVehicleDetailWash@gmail.com</p>
+      <p>https://www.facebook.com/profile.php?id=100092693419212&mibextid=LQQJ4d</p>
+        
     </footer>
   );
 };
